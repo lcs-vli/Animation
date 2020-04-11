3395565a91e4c77b91e0ba4c1384276d979a7e8a
Victoria Li <VLi@lcs.on.ca>
2020-04-11 15:28:42 +0800

    got the four corners to move in the right directions but can't get the colors to work it stays black

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,7 +9,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var x1: Int
     // This function runs once
     override init() {
         
@@ -18,18 +18,37 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        x1 = 250
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        //change position
         x += 1
+        x1 -= 1
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
         
+        // Change color to red
+        canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 75, alpha: 1000)
+        //top right
+        canvas.drawRectangle(at: Point(x: x, y: x), width: 50, height: 50)
+        
+        // Change color to yellow
+        canvas.fillColor = Color(hue: 100, saturation: 100, brightness: 75, alpha: 1000)
+        //botom left
+        canvas.drawRectangle(at: Point(x: x1-40, y: x1-40), width: 50, height: 50)
+        
+        // Change color to blue
+        canvas.fillColor = Color(hue: 240, saturation: 100, brightness: 75, alpha: 1000)
+        //bottom right
+        canvas.drawRectangle(at: Point(x: x, y: x1-40), width: 50, height: 50)
+        
+        // Change color to green
+        canvas.fillColor = Color(hue: 120, saturation: 100, brightness: 75, alpha: 1000)
+        //top left
+        canvas.drawRectangle(at: Point(x: x1-40, y: x), width: 50, height: 50)
     }
     
 }
