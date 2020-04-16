f86d3cea070043a9e1650a25e31904b2e68681c1
Victoria Li <VLi@lcs.on.ca>
2020-04-16 23:34:30 +0800

    tried the covering up with white rectangle method to erase its path but it seems to be drawing all of the back circles and then all of the white rectangles, which just end up being all white. Nothing shows in the animation.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -10,6 +10,12 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    let xstartingPosition = Int(random(from: 10, to: 490))
+    let ystartingPosition = Int(random(from: 10, to: 490))
+    
+    let white = Color(hue:0, saturation: 0, brightness: 100, alpha: 100)
+    let black = Color(hue:0, saturation: 0, brightness: 0, alpha: 100)
+    
     // This function runs once
     override init() {
         
@@ -17,18 +23,29 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
         
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
+
         
         // Change position
-        x += 1
+        x += 10
+        
+        canvas.fillColor = black
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        canvas.drawEllipse(at: Point(x: xstartingPosition + x, y: ystartingPosition + x), width: 50, height: 50)
+        
+        canvas.drawShapesWithBorders = false
+        
+        canvas.fillColor = white
+        
+        canvas.drawRectangle(at: Point(x: xstartingPosition + x - 25, y: ystartingPosition + x - 25), width: 50, height: 50)
+        
+        
         
     }
     
