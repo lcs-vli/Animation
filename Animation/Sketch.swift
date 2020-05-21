6ba5f8f11082a54754f21786cc1dc561f5cc9d66
Victoria Li <VLi@lcs.on.ca>
2020-05-21 15:08:16 +0800

    draw the footprints with one large ellipse and four smaller circles. Also draw the grey circle above. This moves along y=250 Next step: get each shadow to rotate 45 degrees anti-clockwise

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    
     
     // This function runs once
     override init() {
@@ -17,19 +19,38 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = -80
+        y = 250
+        
         
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        canvas.drawShapesWithBorders = false
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        x += 90
+  
+        //draw the grey circle above using grey
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 80, alpha: 50)
+        canvas.drawEllipse(at: Point(x: x - 30, y: y + 30), width: 80, height: 100)
         
+        //change the color back to black
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+        // Draw the big ellipse
+        canvas.drawEllipse(at: Point(x: x, y: 250), width: 80, height:40)
+        
+        // Draw the 4 small ellipses around
+        canvas.drawEllipse(at: Point(x: x + 35, y: y + 25), width: 10, height: 10)
+        canvas.drawEllipse(at: Point(x: x + 45, y: y + 10), width: 10, height: 10)
+        canvas.drawEllipse(at: Point(x: x + 45, y: y - 10), width: 10, height: 10)
+        canvas.drawEllipse(at: Point(x: x + 35, y: y - 25), width: 10, height: 10)
+        
+        // pause for 1 second
+        do {
+            sleep(1)
+        }
     }
     
 }
